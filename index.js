3204f2f529f724d0c8c2a6fbfae9518399f774bb
EchoEkhi <ngprogrammer@gmail.com>
2021-08-01 17:06:44 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+app.listen(process.env.PORT, () => console.log('Server running'))
+
+let count = 0
+
+app.use('/', (_, res) => {
+
+    let url = ''
+
+    switch (count % 4) {
+        case 0:
+            url = process.env.LINK0
+            break
+        case 1:
+            url = process.env.LINK1
+            break
+        case 2:
+            url = process.env.LINK2
+            break
+        case 3:
+            url = process.env.LINK3
+            break
+    }
+
+    res.redirect(url)
+    console.log(`Redirected applicant No.${count}. URL No.${count % 4}.`)
+
+    count++
+
+})
